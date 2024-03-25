cca13345d08ed86b9f4fc5d398bd267999bece49
Zori Badkerhanian <zoribadkerhanian@gmail.com>
2024-03-25 03:05:58 -0700

    linked index.html to app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.secret_key = 'secret_key'
 # MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'C@staneda2024!'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'user'
 
 mysql = MySQL(app)
@@ -41,17 +41,17 @@ def signup():
         # Validate email format
         if not validate_email(email):
             flash('Invalid email format', 'error')
-            return redirect(url_for('signup'))
+            return redirect(url_for('home'))
 
         # Validate password length
         if not validate_password(password):
             flash('Password must be at least 6 characters long', 'error')
-            return redirect(url_for('signup'))
+            return redirect(url_for('home'))
 
         # Check if passwords match
         if password != confirm_password:
             flash('Passwords do not match', 'error')
-            return redirect(url_for('signup'))
+            return redirect(url_for('home'))
 
         # Check if username or email already exists
         cur = mysql.connection.cursor()
@@ -62,7 +62,7 @@ def signup():
 
         if existing_user:
             flash('Username or email already exists', 'error')
-            return redirect(url_for('signup'))
+            return redirect(url_for('home'))
         else:
             # Insert new user into the database
             cur = mysql.connection.cursor()
@@ -70,9 +70,9 @@ def signup():
             mysql.connection.commit()
             cur.close()
             flash('Account created successfully', 'success')
-            return redirect(url_for('login'))
+            return redirect(url_for('home'))
 
-    return render_template('signup.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -92,13 +92,13 @@ def login():
 
         # If a user with matching username and password is found, redirect to home page
         if user:
-            flash('Login successful', 'success')
+            flash(f'Login successful. Welcome {username}!', 'success')
             return redirect(url_for('home'))
         else:
             flash('Invalid username or password', 'error')
 
     # Render the login page
-    return render_template('login.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
