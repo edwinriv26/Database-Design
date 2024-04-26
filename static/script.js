d7bbddc6d87034d755177616ca5ed242085f3b97
Edwin Rivera <edwin.rivera.410@my.csun.edu>
2024-04-25 20:34:28 -0700

    Got rid of old duplicates

--- static/script.js
+++
@@ -1,7 +0,0 @@
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    var modal = document.getElementById('signupModal');
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
